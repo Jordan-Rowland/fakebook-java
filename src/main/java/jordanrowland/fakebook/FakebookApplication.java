31212f972a91d997db1b146660fbcbb6fbe07118
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2024-08-28 10:43:37 -0700

    updates

--- src/main/java/jordanrowland/fakebook/FakebookApplication.java
+++ src/main/java/jordanrowland/fakebook/FakebookApplication.java
@@ -2,9 +2,12 @@ package jordanrowland.fakebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication()
+@EnableJpaRepositories(basePackages="jordanrowland.fakebook.repository")
+@EntityScan(basePackages = "jordanrowland.fakebook.model")
 public class FakebookApplication {
 
 	public static void main(String[] args) {
